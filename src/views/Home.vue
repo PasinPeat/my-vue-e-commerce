a9089e65a63f2d38654688aef80fdd630f27fd71
PasinPeat <pasin.lawn1995@gmail.com>
2024-09-04 10:45:07 +0700

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  <button class="btn">Button</button>
+  <button class="btn btn-neutral">Neutral</button>
+  <button class="btn btn-primary">Primary</button>
+  <button class="btn btn-secondary">Secondary</button>
+  <button class="btn btn-accent">Accent</button>
+  <button class="btn btn-ghost">Ghost</button>
+  <button class="btn btn-link">Link</button>
+</template>
