c13fc76d4e55d4e2091530f1eaf22681592ca4e9
PasinPeat <pasin.lawn1995@gmail.com>
2024-09-12 18:15:17 +0700

    adding deploy config

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "my-vue-e-commerce",
   plugins: [
     vue(),
     vueJsx(),
